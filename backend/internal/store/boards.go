8615476ce5ec72175446e1300db7365eecb7933b
vaidik-bajpai <codervaidik@gmail.com>
2025-11-23 16:20:37 +0530

    enhc: corrected the create board to make it consistent

--- backend/internal/store/boards.go
+++ backend/internal/store/boards.go
@@ -3,19 +3,34 @@ package store
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/vaidik-bajpai/Nexus/backend/internal/db/db"
 	"github.com/vaidik-bajpai/Nexus/backend/internal/types"
 )
 
 func (s *Store) CreateBoard(ctx context.Context, board *types.CreateBoard) error {
-	_, err := s.db.Board.CreateOne(
+	boardID := uuid.New().String()
+	boardTxn := s.db.Board.CreateOne(
 		db.Board.Name.Set(board.Name),
 		db.Board.Owner.Link(
 			db.User.ID.Equals(board.OwnerID),
 		),
+		db.Board.ID.Set(boardID),
 		db.Board.Visibility.SetIfPresent(&board.Visibility),
-	).Exec(ctx)
-	return err
+		db.Board.Background.SetIfPresent(&board.Background),
+	).Tx()
+
+	boardMemTxn := s.db.BoardMember.CreateOne(
+		db.BoardMember.Board.Link(
+			db.Board.ID.Equals(boardID),
+		),
+		db.BoardMember.User.Link(
+			db.User.ID.Equals(board.OwnerID),
+		),
+		db.BoardMember.Role.Set("admin"),
+	).Tx()
+
+	return s.db.Prisma.Transaction(boardTxn, boardMemTxn).Exec(ctx)
 }
 
 func (s *Store) ListBoards(ctx context.Context, ownerID string, paginate *types.Paginate) ([]*types.Board, error) {
